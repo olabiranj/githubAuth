56a9bd78d72e1af012967fb38c1920bf41d61cd9
Olabiran Joshua <olabiranj@gmail.com>
2020-04-18 07:52:20 +0100

    api changed

--- src/App.js
+++ src/App.js
@@ -1,30 +1,29 @@
 import React from "react";
-// import { Provider } from "react-redux";
-// import store from "./store";
-// import Index from "./pages/index";
-// import Dashboard from "./pages/dashboard";
-// import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./store";
+import Index from "./pages/index";
+import Dashboard from "./pages/dashboard";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 const App = () => {
   return (
-    // <Provider store={store}>
-    //   <React.Fragment>
-    //     <Router>
-    //       <Switch>
-    //         <Route exact path="/">
-    //           <Index />
-    //         </Route>
-    //         <Route path="/dashboard">
-    //           <Dashboard />
-    //         </Route>
-    //         <Route path="/test">
-    //           <h1>Hello</h1>
-    //         </Route>
-    //       </Switch>
-    //     </Router>
-    //   </React.Fragment>
-    // </Provider>
-    <h1>hello</h1>
+    <Provider store={store}>
+      <React.Fragment>
+        <Router>
+          <Switch>
+            <Route exact path="/">
+              <Index />
+            </Route>
+            <Route path="/dashboard">
+              <Dashboard />
+            </Route>
+            <Route path="/test">
+              <h1>Hello</h1>
+            </Route>
+          </Switch>
+        </Router>
+      </React.Fragment>
+    </Provider>
   );
 };
 
