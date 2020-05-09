fe9ff135376ab2d3c3c4095ee5e83fc2d8cd4013
Olabiran Joshua <olabiranj@gmail.com>
2020-05-09 19:04:08 +0100

    dev tools removed

--- src/store.js
+++ src/store.js
@@ -1,4 +1,4 @@
-import { createStore, applyMiddleware, compose } from "redux";
+import { createStore, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 import rootReducer from "./reducers";
 
@@ -6,13 +6,12 @@ const initialState = {};
 
 const middleWare = [thunk];
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+// const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-const devTools =
-  process.env.NODE_ENV === "production"
-    ? applyMiddleware(...middleWare)
-    : composeEnhancers(applyMiddleware(...middleWare));
-
-const store = createStore(rootReducer, initialState, devTools);
+const store = createStore(
+  rootReducer,
+  initialState,
+  applyMiddleware(...middleWare)
+);
 
 export default store;
